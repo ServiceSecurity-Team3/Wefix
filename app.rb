75b15b47694b6f3886d026dab7fdf7471e4d587a
Abner Tellez <atellezsazo@gmail.com>
2018-04-26 15:49:28 +0800

    Add database models to the endpoints
    - Change API

--- app.rb
+++ app.rb
@@ -2,54 +2,95 @@
 
 require 'roda'
 require 'json'
-require 'base64'
 
+require_relative 'config/environments'
 require_relative 'models/init'
 
 module Wefix
-  # Web controller for Location API
+  # Web controller for Wefix API
   class Api < Roda
-    plugin :environments
     plugin :halt
-
-    configure do
-      Location.setup
-    end
 
     route do |routing|
       response['Content-Type'] = 'application/json'
 
       routing.root do
-        { message: 'Location up at /api/v1' }.to_json
+        { message: 'Wefix API up at /api/v1' }.to_json
       end
 
       routing.on 'api' do
         routing.on 'v1' do
-          routing.on 'locations' do
-            routing.get String do |id|
-              Location.find(id).to_json
-            rescue StandardError
-              routing.halt 404, { message: 'Location not found' }.to_json
-            end
+          @api_root = "api/v1"
 
-            # GET api/v1/locations
-            routing.get do
-              output = { location_ids: Location.all }
-              JSON.pretty_generate(output)
-            end
+          routing.on 'groups' do
+            @proj_route = "#{@api_root}/groups"
 
-            # POST api/v1/locations
-            routing.post do
-              data = JSON.parse(routing.body.read)
-              location = Location.new(data)
-              if location.save
-                response.status = 201
-                { message: 'Location saved', id: location.id }.to_json
-              else
-                routing.halt 400, { message: 'Could not save Location' }.to_json
+            routing.on String do |group_id|
+              routing.on 'problems' do
+                @doc_route = "#{@api_root}/groups/#{group_id}/problems"
+                # GET api/v1/groups/[group_id]/problems/[pro_id]
+                routing.get String do |pro_id|
+                  pro = Problem.where(group_id: group_id, id: pro_id).first
+                  pro ? pro.to_json : raise('Problem not found')
+                rescue StandardError => error
+                  routing.halt 404, { message: error.message }.to_json
+                end
+
+                # GET api/v1/groups/[group_id]/problems
+                routing.get do
+                  output = { data: Group.first(id: group_id).problems }
+                  JSON.pretty_generate(output)
+                rescue StandardError
+                  routing.halt 404, { message: 'Could not find problems' }
+                end
+
+                # POST api/v1/groups/[ID]/problems
+                routing.post do
+                  new_data = JSON.parse(routing.body.read)
+                  group = Group.first(id: proj_id)
+                  new_doc = group.add_problem(new_data)
+
+                  if new_doc
+                    response.status = 201
+                    response['Location'] = "#{@doc_route}/#{new_doc.id}"
+                    { message: 'Problem saved', data: new_doc }.to_json
+                  else
+                    routing.halt 400, 'Could not save the problem'
+                  end
+
+                rescue StandardError
+                  routing.halt 500, { message: 'Database error' }.to_json
+                end
               end
+
+              # GET api/v1/groups/[ID]
+              routing.get do
+                group = Group.first(id: group_id)
+                group ? group.to_json : raise('Group not found')
+              rescue StandardError => error
+                routing.halt 404, { message: error.message }.to_json
+              end
+            end
+
+            # GET api/v1/groups
+            routing.get do
+              output = { data: Group.all }
+              JSON.pretty_generate(output)
             rescue StandardError
-              routing.halt 400, { message: 'Error: Invalid request' }.to_json
+              routing.halt 404, { message: 'Could not find groups' }.to_json
+            end
+
+            # POST api/v1/groups
+            routing.post do
+              new_data = JSON.parse(routing.body.read)
+              new_group = Group.new(new_data)
+              raise('Could not save the group') unless new_group.save
+
+              response.status = 201
+              response['Location'] = "#{@proj_route}/#{new_group.id}"
+              { message: 'Group saved', data: new_group }.to_json
+            rescue StandardError => error
+              routing.halt 400, { message: error.message }.to_json
             end
           end
         end
