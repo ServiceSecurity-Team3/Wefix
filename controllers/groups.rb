5f65fe081372ee5b26ed5548931aaca400d1a9d0
penestia <elona.shatri@studentet.uni-pr.edu>
2018-06-21 02:23:51 +0800

    rubocop

--- controllers/groups.rb
+++ controllers/groups.rb
@@ -42,7 +42,7 @@ module Wefix
         rescue Sequel::MassAssignmentRestriction
           routing.halt 400, {message: "Illegal Request"}.to_json
         rescue StandardError => error
-          routing.halt 500, {message: error.message}.to_json
+          routing.halt 500, { message: error.message }.to_json
         end
 
         # GET api/v1/groups/[grp_id]
@@ -56,37 +56,37 @@ module Wefix
                .merge(policies: policy.summary)
                .to_json
         rescue StandardError
-          routing.halt 404, {message: "Group not found"}.to_json
+          routing.halt 404, { message: 'Group not found' }.to_json
         end
       end
 
       # GET api/v1/groups
       routing.get do
-        account = Account.first(username: @auth_account["username"])
+        account = Account.first(username: @auth_account['username'])
         groups_scope = GroupPolicy::AccountScope.new(account)
         viewable_groups = groups_scope.viewable
 
         JSON.pretty_generate(viewable_groups)
       rescue StandardError
-        routing.halt 403, {message: "Could not find groups"}.to_json
+        routing.halt 403, { message: 'Could not find groups' }.to_json
       end
 
       # POST api/v1/groups
       routing.post do
-        account = Account.first(username: @auth_account["username"])
+        account = Account.first(username: @auth_account['username'])
         group_data = JSON.parse(routing.body.read)
         new_group = Wefix::CreateGroupForOwner.call(
-          owner_id: account.id, group_data: group_data,
+          owner_id: account.id, group_data: group_data
         )
-        raise("Could not save group") unless new_group.save
+        raise('Could not save group') unless new_group.save
 
         response.status = 201
-        response["Location"] = "#{@grp_route}/#{new_group.id}"
-        {message: "Group saved", data: new_group}.to_json
+        response['Location'] = "#{@grp_route}/#{new_group.id}"
+        { message: 'Group saved', data: new_group }.to_json
       rescue Sequel::MassAssignmentRestriction
-        routing.halt 400, {message: "Illegal Request"}.to_json
+        routing.halt 400, { message: 'Illegal Request' }.to_json
       rescue StandardError => error
-        routing.halt 500, {message: error.message}.to_json
+        routing.halt 500, { message: error.message }.to_json
       end
     end
   end
--- specs/account_spec.rb
+++ specs/account_spec.rb
@@ -1,94 +1,94 @@
 # frozen_string_literal: true
 
-require_relative "./spec_helper"
+require_relative './spec_helper'
 
-describe "Test Document Handling" do
+describe 'Test Document Handling' do
   include Rack::Test::Methods
 
   before do
     wipe_database
-    @req_header = {"CONTENT_TYPE" => "application/json"}
+    @req_header = { 'CONTENT_TYPE' => 'application/json' }
   end
 
-  describe "Account information" do
-    it "HAPPY: should be able to get details of a single account" do
+  describe 'Account information' do
+    it 'HAPPY: should be able to get details of a single account' do
       account_data = DATA[:accounts][1]
       account = Wefix::EmailAccount.create(account_data)
 
-      get "/api/v1/accounts/#{account.username}"
+      get { '/api/v1/accounts/#{account.username' }
       _(last_response.status).must_equal 200
 
       result = JSON.parse last_response.body
-      _(result["username"]).must_equal account.username
-      _(result["salt"]).must_be_nil
-      _(result["password"]).must_be_nil
-      _(result["password_hash"]).must_be_nil
+      _(result['username']).must_equal account.username
+      _(result['salt']).must_be_nil
+      _(result['password']).must_be_nil
+      _(result['password_hash']).must_be_nil
     end
   end
 
-  describe "Account Creation" do
+  describe 'Account Creation' do
     before do
       @account_data = DATA[:accounts][1]
     end
 
-    it "HAPPY: should be able to create new email accounts" do
-      post "api/v1/accounts", @account_data.to_json, @req_header
+    it 'HAPPY: should be able to create new email accounts' do
+      post 'api/v1/accounts', @account_data.to_json, @req_header
       _(last_response.status).must_equal 201
-      _(last_response.header["Location"].size).must_be :>, 0
+      _(last_response.header['Location'].size).must_be :>, 0
 
-      created = JSON.parse(last_response.body)["data"]
+      created = JSON.parse(last_response.body)['data']
       account = Wefix::EmailAccount.first
 
-      _(created["username"]).must_equal @account_data["username"]
-      _(created["email"]).must_equal @account_data["email"]
-      _(account.password?(@account_data["password"])).must_equal true
-      _(account.password?("not_really_the_password")).must_equal false
+      _(created['username']).must_equal @account_data['username']
+      _(created['email']).must_equal @account_data['email']
+      _(account.password?(@account_data['password'])).must_equal true
+      _(account.password?('not_really_the_password')).must_equal false
     end
 
-    it "BAD: should not create account with illegal attributes" do
+    it 'BAD: should not create account with illegal attributes' do
       bad_data = @account_data.clone
-      bad_data["created_at"] = "1900-01-01"
-      post "api/v1/accounts", bad_data.to_json, @req_header
+      bad_data['created_at'] = '1900-01-01'
+      post 'api/v1/accounts', bad_data.to_json, @req_header
 
       _(last_response.status).must_equal 400
-      _(last_response.header["Location"]).must_be_nil
+      _(last_response.header['Location']).must_be_nil
     end
   end
 
-  describe "Account Authentication" do
+  describe 'Account Authentication' do
     before do
       @account_data = DATA[:accounts][1]
       @account = Wefix::EmailAccount.create(@account_data)
     end
 
-    it "HAPPY: should authenticate valid credentials" do
-      credentials = {username: @account_data["username"],
-                     password: @account_data["password"]}
-      post "api/v1/auth/authenticate", credentials.to_json, @req_header
+    it 'HAPPY: should authenticate valid credentials' do
+      credentials = { username: @account_data['username'],
+                      password: @account_data['password'] }
+      post 'api/v1/auth/authenticate', credentials.to_json, @req_header
 
       _(last_response.status).must_equal 200
       auth_account = JSON.parse(last_response.body)
       _(last_response.status).must_equal 200
-      account_response = auth_account["account"]
-      _(account_response["username"].must_equal(@account_data["username"]))
-      _(account_response["email"].must_equal(@account_data["email"]))
-      _(account_response["id"].must_be_nil)
+      account_response = auth_account['account']
+      _(account_response['username'].must_equal(@account_data['username']))
+      _(account_response['email'].must_equal(@account_data['email']))
+      _(account_response['id'].must_be_nil)
     end
 
-    it "BAD: should not authenticate invalid password" do
-      credentials = {username: @account_data["username"],
-                     password: "fakepassword"}
+    it 'BAD: should not authenticate invalid password' do
+      credentials = { username: @account_data['username'],
+                      password: 'fakepassword' }
 
-      assert_output(/invalid/i, "") do
-        post "api/v1/accounts/authenticate", credentials.to_json, @req_header
+      assert_output(/invalid/i, '') do
+        post 'api/v1/accounts/authenticate', credentials.to_json, @req_header
       end
 
       result = JSON.parse(last_response.body)
 
       _(last_response.status).must_equal 403
-      _(result["message"]).wont_be_nil
-      _(result["username"]).must_be_nil
-      _(result["email"]).must_be_nil
+      _(result['message']).wont_be_nil
+      _(result['username']).must_be_nil
+      _(result['email']).must_be_nil
     end
   end
 end
